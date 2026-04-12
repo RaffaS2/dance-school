0a2e34832e7f1384822e4dc51382ad74e67b4088
Dantaslindoo <a27958@alunos.ipca.pt>
2026-04-12 15:27:11 +0100

    primeira commit teste Bruno Dantas

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,6 +8,7 @@ async function getData(){
 async function home() {
   const data = await getData()
   return (
+    //teste
     <div className='min-h-screen bg-blue-950 flex items-center justify-center'>
       <h1 className='text-white text-4xl font-bold'>{data.message}</h1>
       <Link href="/dashboard">go to dashboard</Link>
