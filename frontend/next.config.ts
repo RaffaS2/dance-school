fc45f58b69e2a7d3199c8c933060e4a0b7bbc3c2
Rafael Soares <151098442+RaffaS2@users.noreply.github.com>
2026-04-14 21:15:21 +0100

    test_update_next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    root: __dirname, //diz ao Turbopack que a raiz do projeto é a pasta frontend/, eliminando a confusão com o package-lock.json da raiz.
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
