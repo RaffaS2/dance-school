1a5cbf028342ee7c98cf4b3ff64c4676c85a29c0
Rafael Soares <151098442+RaffaS2@users.noreply.github.com>
2026-04-14 22:34:44 +0100

    test_next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: __dirname, //diz ao Turbopack que a raiz do projeto é a pasta frontend/, eliminando a confusão com o package-lock.json da raiz.
+    root: "./", //diz ao Turbopack que a raiz do projeto é a pasta frontend/, eliminando a confusão com o package-lock.json da raiz.
   },
 };
 
