5da6b38fbfe6a0897bf4ee4f4e74c3cee395207f
RaffaS2 <rafael.r4ff@gmail.com>
2026-04-21 12:09:48 +0100

    update file distribution

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,69 @@
+//COMPLETE CRUD HERE - CREATE, READ, UPDATE AND DELETE
+
+const pool = require('../db')
+
+// create user
+const createUser = async (req, res) => {
+    try {
+      const { name, email, phone_number, id_user_type } = req.body
+      const result = await pool.query(
+        'INSERT INTO users (name, email, phone_number, id_user_type) VALUES ($1, $2, $3, $4) RETURNING *',
+        [name, email, phone_number, id_user_type]
+      )
+      res.status(201).json(result.rows[0])
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  }
+
+// read all users
+const readAllUsers = async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users')
+    res.json(result.rows)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+// read a user by id
+const readUserById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const result = await pool.query('SELECT * FROM users WHERE id_user = $1', [id])
+    res.json(result.rows)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+// update user
+const updateUser = async (req, res) => {
+    try{
+      const { id } = req.params
+      const { name, email, phone_number, id_user_type } = req.body
+      const result = await pool.query(
+        'UPDATE users SET name = $1, email = $2, phone_number = $3, id_user_type = $4 WHERE id_user = $5 RETURNING *',
+        [name, email, phone_number, id_user_type, id]
+      )
+      res.status(200).json(result.rows[0])
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  }
+
+// delete user
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params
+        const result = await pool.query(
+            'DELETE FROM users WHERE id_user = $1 RETURNING *',
+            [id]
+        )
+        res.status(204).json(result.rows[0])
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+module.exports = { createUser, readAllUsers, readUserById, updateUser, deleteUser }
