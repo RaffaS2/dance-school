cb6150c82c0079658deb9891f124089714357601
Rafael Soares <151098442+RaffaS2@users.noreply.github.com>
2026-06-01 16:43:51 +0100

    render_update_apiBase.ts

--- frontend/app/lib/apiBase.ts
+++ frontend/app/lib/apiBase.ts
@@ -1,7 +1,11 @@
-export function getApiBase() {
+/*export function getApiBase() {
 	if (typeof window !== "undefined") {
 		return `http://${window.location.hostname}:3001`;
 	}
  
+	return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
+}*/
+
+export function getApiBase() {
 	return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
 }
