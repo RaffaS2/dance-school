c80055e7e670bf29754e880ca9e8306fd7b844fc
NunoGajoLesi <a23002@alunos.ipca.pt>
2026-06-01 22:57:15 +0100

    removing local host reference

--- frontend/app/lib/apiBase.ts
+++ frontend/app/lib/apiBase.ts
@@ -7,5 +7,5 @@
 }*/
 
 export function getApiBase() {
-	return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
+	return process.env.NEXT_PUBLIC_API_BASE_URL || "https://dance-school-dw6q.onrender.com";
 }
