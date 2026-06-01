4a2e324219e800b4e3a64f2923aa77b62aa54f16
Rafael Soares <151098442+RaffaS2@users.noreply.github.com>
2026-06-01 17:30:02 +0100

    render_authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -137,7 +137,7 @@ exports.login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'none', // o 'lax' bloqueia o cookie em cross-site requests
+      sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
 
@@ -262,11 +262,7 @@ exports.changePassword = async (req, res) => {
 
 // Logout
 exports.logout = (req, res) => {
-  res.clearCookie('token', {
-  httpOnly: true,
-  secure: true,
-  sameSite: 'none',
-})
+  res.clearCookie('token')
   return res.status(200).json({ message: 'Logout com sucesso.' })
 }
 
